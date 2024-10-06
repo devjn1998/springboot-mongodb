3c6fb3c985d00cb9a573ca34c8a82ac1169b2db6
Juan Costa <juandev1998@gmail.com>
2024-10-06 16:23:10 -0300

    Project Created

---
+++ src/main/java/com/juancosta/workshopmongo/WorkshopspringmootmongodobApplication.java
@@ -0,0 +1,13 @@
+package com.juancosta.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopspringmootmongodobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopspringmootmongodobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juancosta/workshopmongo/WorkshopspringmootmongodobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juancosta.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringmootmongodobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
